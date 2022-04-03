b7110b7c8aee1b26fa1f4290eda5b46bd5ce5568
zaknafean <zaknafean@gmail.com>
2022-04-03 10:59:52 -0400

    Recut of Player Sprite

--- enemies/ChaseEnemy.gd
+++ enemies/ChaseEnemy.gd
@@ -69,6 +69,7 @@ func anim_switch(animation, speed = 1):
 func _on_Hit_Box_body_entered(body):
 	if body.name == "Player" and !amEating and amActive:
 		print("EATING")
+		body.taking_damage = true
 		Settings.adjust_sanity(-25)
 		eatTimer.start()
 		amEating = true
