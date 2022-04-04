a821fb41e806bbb794db8291b633dd2ccfea97a8
zaknafean <zaknafean@gmail.com>
2022-04-04 17:25:41 -0400

    Cat fix
    First cat doesn't despawn

--- levels/CatLampTalk.gd
+++ levels/CatLampTalk.gd
@@ -2,18 +2,17 @@ extends SpriteClickable
 
 
 func setup():
-	randomize()
-	var whichCat = randi() % 3 + 1
-	if whichCat < 3:
-		$Sprite.visible = true
-	else:
-		queue_free()
-		return
-	
 	if Settings.firstCat == false:
 		$EventQueue/DialogEvent.DIALOG = 'Friendly? Cat'
 		Settings.firstCat = true
 	else:
+		randomize()
+		var whichCat = randi() % 3 + 1
+		if whichCat < 3:
+			$Sprite.visible = true
+		else:
+			queue_free()
+			return
 		randomize()
 	
 		var ranVal = randi() % 3 +1
