7bb81f1e53d87e68568e3229b2c492df98619683
zaknafean <zaknafean@gmail.com>
2022-04-08 16:20:03 -0400

    Bug Patch 2
    Added some shout outs
    Fixed monsters being hidden from you under rugs
    Fixed lights being clickable forever
    Added missing Fonts attributions
    Fixed bug where Enemy could forget he can hit you

--- levels/floatingtext/Floating_Text.gd
+++ levels/floatingtext/Floating_Text.gd
@@ -1,25 +1,28 @@
 extends Position2D
 
+onready var label = $Label
+onready var tween = $Tween
+
 func show_value(value, travel, duration, spread, type):
 	randomize()
 	
 	var movement = travel.rotated(rand_range(-spread/2, spread/2))
-	$Label.rect_pivot_offset = $Label.rect_size / 2
+	label.rect_pivot_offset = label.rect_size / 2
 	
-	$Tween.interpolate_property($Label, 'rect_position', $Label.rect_position, $Label.rect_position + movement, duration, Tween.TRANS_LINEAR, $Tween.EASE_IN_OUT)
-	$Tween.interpolate_property($Label, 'modulate:a', 1.0, 0.0, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.interpolate_property(label, 'rect_position', label.rect_position, label.rect_position + movement, duration, Tween.TRANS_LINEAR, $Tween.EASE_IN_OUT)
+	tween.interpolate_property(label, 'modulate:a', 1.0, 0.0, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	
 	if type == 0:
-		$Label.text = value + ' Score'
-		$Label.add_color_override("font_color", Color (0.8, .6, .25)) 
-		$Tween.interpolate_property($Label, 'rect_scale', $Label.rect_scale*1.5, $Label.rect_scale*3, 0.4, Tween.TRANS_BACK, Tween.EASE_IN)
+		label.text = value + ' Score'
+		label.add_color_override("font_color", Color (0.8, .6, .25)) 
+		tween.interpolate_property(label, 'rect_scale', label.rect_scale*1.5, label.rect_scale*3, 0.4, Tween.TRANS_BACK, Tween.EASE_IN)
 	if type == 1:
-		$Label.text = value + ' Alert'
-		$Label.add_color_override("font_color", Color (.65, .2, .2))
-		$Tween.interpolate_property($Label, 'rect_scale', $Label.rect_scale*2, $Label.rect_scale, 0.4, Tween.TRANS_BACK, Tween.EASE_IN)
+		label.text = value + ' Alert'
+		label.add_color_override("font_color", Color (.65, .2, .2))
+		tween.interpolate_property(label, 'rect_scale', label.rect_scale*2, label.rect_scale, 0.4, Tween.TRANS_BACK, Tween.EASE_IN)
 	if type == 2:
-		$Label.text = value + ' Sanity'
-		$Label.add_color_override("font_color", Color (.75, .3, .6))
+		label.text = value + ' Sanity'
+		label.add_color_override("font_color", Color (.75, .3, .6))
 	
 	
 	$Tween.start()
